42bb589f53041efc58388e1e5b667bd8ee2a6244
s2516268 <e.a.devree@student.utwente.nl>
2024-11-20 10:28:14 +0100

    Extract Question and subclasses from QuestionParser

--- parser/src/main/kotlin/Main.kt
+++ parser/src/main/kotlin/Main.kt
@@ -32,38 +32,3 @@ fun main() {
         println("=".repeat(50)) // Separator for readability
     }
 }
-
-
-fun main2() {
-    val input = """
-        *** 
-        <!-- Automatically generated ID for database reference. Do not modify!-->
-        <!-- id: unique-question-id-12345!-->
-        type: multiple-choice
-        tags: 
-        - Frontend Developer 
-        - Backend Developer
-        description: What is the difference between a stack and a queue?
-        options: 
-        - [ ] A stack is FIFO, a queue is LIFO.
-        - [x] A stack is LIFO, a queue is FIFO.
-        - [ ] Both are FIFO.
-        - [ ] Both are LIFO.
-        ***
-        
-        *** 
-        <!-- Automatically generated ID for database reference. Do not modify!-->
-        <!-- id: unique-question-id-35842!-->
-        type: open
-        tags: 
-        - Deezend 
-        - developer 
-        - Reee
-        description: What is the difference between a stack and a queue?
-        
-        ***
-        """
-
-    val questionParser = QuestionParser()
-    println(questionParser.parseQuestions(input))
-}
--- parser/src/main/kotlin/QuestionParser.kt
+++ parser/src/main/kotlin/QuestionParser.kt
@@ -1,37 +1,10 @@
 package ut.isep
 
-import java.util.*
+import MultipleChoiceQuestion
+import OpenQuestion
+import Option
+import Question
 
-sealed class Question {
-    abstract val id: String? // Unique identifier for the question, can be null for new questions
-    abstract val type: QuestionType
-    abstract val tags: List<String>
-    abstract val description: String
-}
-
-enum class QuestionType {
-    MULTIPLE_CHOICE, OPEN
-}
-
-data class MultipleChoiceQuestion(
-    override val id: String?,
-    override val type: QuestionType = QuestionType.MULTIPLE_CHOICE,
-    override val tags: List<String>,
-    override val description: String,
-    val options: List<Option> // List of options for the multiple-choice question
-) : Question()
-
-data class OpenQuestion(
-    override val id: String?,
-    override val type: QuestionType = QuestionType.OPEN,
-    override val tags: List<String>,
-    override val description: String
-) : Question()
-
-data class Option(
-    val text: String, // Option text
-    val isCorrect: Boolean // True if the option is correct, otherwise false
-)
 
 class QuestionParser {
 
