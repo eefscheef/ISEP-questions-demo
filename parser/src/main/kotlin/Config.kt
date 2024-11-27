e1ee2cf75b52c52333831ea6149f37c522476627
s2516268 <e.a.devree@student.utwente.nl>
2024-11-27 16:32:41 +0100

    add jackson annotated class for parsing config.yaml

---
+++ parser/src/main/kotlin/Config.kt
@@ -0,0 +1,9 @@
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Config @JsonCreator constructor(
+    @JsonProperty("tag-options")
+    val tagOptions: List<String>,
+    @JsonProperty("question-options")
+    val questionOptions: List<String>,
+)
