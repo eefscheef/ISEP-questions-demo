1eb7ca77767b5190e861fc0101d0ae29f1fd6ee9
everard <e.a.devree@student.utwente.nl>
2024-12-04 15:34:18 +0100

    Factor out repository walking into AssessmentProcessor

---
+++ uploader/parser/src/main/kotlin/AssessmentProcessor.kt
@@ -0,0 +1,80 @@
+package ut.isep
+
+import Config
+import question.Question
+import QuestionParser
+import java.io.File
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import ut.isep.management.model.entity.Assessment
+import ut.isep.management.model.entity.Section
+
+class AssessmentProcessor(val rootDir: File, val configFile: File) {
+
+    val config: Config = parseConfig()
+
+    private fun parseConfig(): Config {
+        val mapper = ObjectMapper(YAMLFactory())
+        return mapper.readValue(configFile, Config::class.java)
+    }
+
+    private fun getQuestionDirectories(): Array<File> {
+        return rootDir.listFiles { file ->
+            file.isDirectory &&
+                    file.name != "uploader" &&
+                    !file.name.startsWith(".")
+        } ?: emptyArray()
+    }
+
+    private fun parseMarkDownFilesInDir(
+        parser: QuestionParser,
+        questionDir: File
+    ): Map<String, Section> {
+        val mdFiles = questionDir.listFiles { file -> file.extension == "md" } ?: emptyArray()
+        val questions: List<Question> = mdFiles.map { mdFile ->
+            parser.parseQuestion(mdFile.readText())
+        }
+        val questionsByTag = questions.flatMap { question ->
+            question.tags.map { tag -> tag to question }
+        }.groupBy({ it.first }, { it.second })
+
+        return questionsByTag.mapValues { (tag, questions) ->
+            questions.toSection()
+        }
+    }
+
+    private fun List<Question>.toSection(): Section {
+        return Section(
+            assignments = this.map { question ->
+                question.toEntity()
+            }
+        )
+    }
+
+    fun printQuestions(questions: List<Question>) {
+        questions.forEach { question ->
+            println(question)
+        }
+        println("=".repeat(50))
+    }
+
+
+    fun process(): List<Assessment> {
+        val questionDirs = getQuestionDirectories()
+        val parser = QuestionParser(config)
+        val tagsToSections: Map<String, List<Section>> = questionDirs.map { questionDir ->
+            parseMarkDownFilesInDir(parser, questionDir)
+        }
+        .flatMap { it.entries } // List<String, Section>
+        .groupBy({ it.key }, { it.value }) // Map<String, List<Section>>
+
+        return tagsToSections.map {(tag, sections) ->
+            val assessment = Assessment(id = 0, sections = sections.toMutableList())
+            assessment.sections.forEach {
+                section -> section.assessment = assessment
+            }
+            assessment
+        }
+    }
+}
--- uploader/parser/src/main/kotlin/Main.kt
+++ uploader/parser/src/main/kotlin/Main.kt
@@ -1,59 +1,10 @@
 package ut.isep
 
-import Config
-import question.Question
-import QuestionParser
 import java.io.File
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-
-
-fun parseConfig(filePath: String): Config {
-    val mapper = ObjectMapper(YAMLFactory())
-    return mapper.readValue(File(filePath), Config::class.java)
-}
-
-fun getQuestionDirectories(rootDir: File): Array<File> {
-    return rootDir.listFiles {
-        file -> file.isDirectory &&
-            file.name != "uploader" &&
-            !file.name.startsWith(".")
-    } ?: emptyArray()
-}
-
-fun processMarkdownFilesInDir(parser: QuestionParser,
-                              questionDir: File,
-                              questionsByTag: Map<String, MutableList<Question>>) {
-    val mdFiles = questionDir.listFiles { file -> file.extension == "md" } ?: emptyArray()
-    println("Topic: ${questionDir.name}")
-
-    val allQuestions: List<Question> = mdFiles.map { mdFile ->
-        println("Questions in ${mdFile.name}:")
-        val content = mdFile.readText()
-        parser.parseQuestion(content)
-    }
-    
-    for (question in allQuestions) {
-            println(question)
-            question.tags.forEach {tag ->
-                questionsByTag.get(tag)?.add(question) ?:
-                throw Exception("Unknown tag provided: ${tag} in question ${question}")
-            }
-        }
-    println("=".repeat(50)) // Separator for readability
-}
-
-
 fun main() {
-    val config = parseConfig("../config.yaml")
-    val tagToQuestions: Map<String, MutableList<Question>> = config.tagOptions.associateWith { mutableListOf() }
-
-    val questionDirs = getQuestionDirectories(File(".."))
-
-    val parser = QuestionParser()
-
-    questionDirs.forEach { questionDir ->
-        processMarkdownFilesInDir(parser, questionDir, tagToQuestions)
-    }
+    val rootDir = File(".")
+    val configFile: File = rootDir.resolve("config.yaml")
+    val assessments = AssessmentProcessor(rootDir, configFile).process()
+    println(assessments)
 }
