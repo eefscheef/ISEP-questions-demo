69aa1f17327d8566e5bd362b4b26a3d08e8f824e
s2516268 <e.a.devree@student.utwente.nl>
2025-01-21 12:15:24 +0100

    Move QuestionParser and parsed Question files into shared-entities

--- uploader/parser/src/main/kotlin/Config.kt
+++
@@ -1,9 +0,0 @@
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class Config @JsonCreator constructor(
-    @JsonProperty("tag-options")
-    val tagOptions: List<String>,
-    @JsonProperty("question-options")
-    val questionOptions: List<String>,
-)
